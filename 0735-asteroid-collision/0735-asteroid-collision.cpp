f33e70359b4aae0705d8147dafb3f898316194e8
Abhishekh Mishra <153365622+Abhishekhmishra0@users.noreply.github.com>
2026-07-30 18:49:58 +0530

    Time: 0 ms (100%), Space: 22 MB (60.75%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool isDiff(int x, int y) {
-        return ((x < 0 && y > 0) || (x > 0 && y < 0));
-    }
     vector<int> asteroidCollision(vector<int>& arr) {
         int n = arr.size();
         stack<int> st;
         for(int i = 0; i < n; i++) {
-            if(!st.empty() && isDiff(st.top(), arr[i]) && st.top() > 0 && arr[i] < 0 && abs(st.top()) == abs(arr[i])) {
-                st.pop();
-                continue;
-            }
-            bool check = true;
-            while(!st.empty() && st.top() > 0 && arr[i] < 0 && abs(st.top()) <= abs(arr[i])) {
-                 if(abs(st.top()) == abs(arr[i])) {
-                    st.pop();
-                    check = false;
+            bool check = false;
+            while(!st.empty() && st.top() > 0 && arr[i] < 0) {
+                if(abs(st.top()) > abs(arr[i])) {
+                    check = true;
                     break;
                 }
-                st.pop();
+                else if(abs(st.top()) == abs(arr[i])) {
+                    check = true;
+                    st.pop();
+                    break;
+                }
+                else {
+                    st.pop();
+                }
             }
-            if(!check) continue;
-            if(!st.empty() && st.top() > 0 && arr[i] < 0 && abs(st.top()) > abs(arr[i])) {
-                continue;
-            }
+            if(check) continue;
             st.push(arr[i]);
         }
         vector<int> ans;
